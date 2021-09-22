73b9fc83b6e3de296d857bfce8b089d9652421fd
xtonx00 <87303487+xtonx00@users.noreply.github.com>
2021-09-22 15:23:30 +0200

    Add files via upload

--- src/app/features/page-routing/page-routing.module.ts
+++ src/app/features/page-routing/page-routing.module.ts
@@ -6,7 +6,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    //redirectTo: 'homepage',
+    // redirectTo: 'homepage',
     pathMatch: 'full',
     component: HomepageComponent
   },
